1c95b95f4d253108cc22f055e0b87c9e12a6312b
Owen Luu <owenluu111@gmail.com>
2021-01-12 20:25:17 -0600

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+my_product = Product.new({ name: "Air Jordan 1 Chicago", price: 1800, image_url: "https://stockx-360.imgix.net/Air-Jordan-1-Retro-Chicago-2015/Images/Air-Jordan-1-Retro-Chicago-2015/Lv2/img04.jpg?auto=format,compress&q=90&updated_at=1607656901&w=1000", description: "2015 size 10" })
+my_product.save
+
+my_product = Product.new({ name: "Air Jordan 4 Bred 4", price: 455, image_url: "https://stockx-360.imgix.net/Air-Jordan-4-Retro-Bred-2019/Images/Air-Jordan-4-Retro-Bred-2019/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606316608&w=1000", description: "2019 size 10" })
+my_product.save
+
+my_product = Product.new({ name: "Air Jordan 6 Infared", price: 318, image_url: "https://stockx-360.imgix.net/Air-Jordan-6-Retro-Black-Infrared-2019/Images/Air-Jordan-6-Retro-Black-Infrared-2019/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606315701&w=1000", description: "2019 size 10" })
+my_product.save
+
+my_product = Product.new({ name: "Air Jordan 11 Concord", price: 410, image_url: "https://stockx-360.imgix.net/Air-Jordan-11-Retro-Concord-2018/Images/Air-Jordan-11-Retro-Concord-2018/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606320605&w=1000", description: "2018 size 10" })
+my_product.save
+
+my_product = Product.new({ name: "Air Jordan 11 Breds/Playoffs", price: 369, image_url: "https://stockx-360.imgix.net/Air-Jordan-11-Retro-Playoffs-2019/Images/Air-Jordan-11-Retro-Playoffs-2019/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606320266&w=1000", description: "2019, size 10" })
+my_product.save
+
+my_product = Product.new({ name: "Air Jordan 11 Space Jam", price: 440, image_url: "https://stockx-360.imgix.net/Air-Jordan-11-Retro-Space-Jam-2016/Images/Air-Jordan-11-Retro-Space-Jam-2016/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1606319512&w=1000", description: "2016 size 10" })
+my_product.save
