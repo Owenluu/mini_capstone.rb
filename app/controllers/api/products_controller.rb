61d6da84ed4d28942bc7da72c2fb69875ca692ab
Owen Luu <owenluu111@gmail.com>
2021-01-14 21:26:12 -0600

    Added all routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,38 @@
 class Api::ProductsController < ApplicationController
-  def all_product_method
-    @product = Product.all
-    render "all_product.json.jb"
+  def index
+    @products = Product.all
+    render "index.json.jb"
+  end
+
+  def show
+    @products = Product.find_by(id: params["id"])
+    render "show.json.jb"
+  end
+
+  def create
+    @products = Product.new{
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    }
+    @products.save
+    render "show.json.jb"
+  end
+
+  def update
+    @products = Product.find_by(id: params[:id])
+    @products.name = params[:name] || @product.name
+    @products.price = params[:price] || @product.price
+    @products.image_url = params[:image_url] || @product.image_url
+    @products.description = params[:description] || @product.description
+    @products.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    products = Product.find_by(id: params[:id])
+    products.destory
+    render json: { message: "Products sucessfully destroyed!" }
   end
 end
